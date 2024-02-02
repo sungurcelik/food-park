d2a75e32b23c1f57730bf5f4017309f6e20f56fd
SalihSungurCelik <salihcelik099@gmail.com>
2024-02-02 17:45:13 +0300

    1

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -11,5 +11,6 @@ class AdminDashboardController extends Controller
     function index(): View
     {
         return view('admin.dashboard.index');
+        // return view('admin.layouts.master');
     }
 }
